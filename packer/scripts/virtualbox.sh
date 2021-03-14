0e523ea0f6ad7ca60e103e952a2cb4c69c0098b0
Brennan Fee <brennan+gitlab@mailbots.org>
2021-03-14 13:48:53 -0500

    feat: Added installation of vbox guest tools
    This now automates the installation of the VirtualBox guest additions.
    I am using the ISO file for doing so rather than using he built-in
    packages for two reasons.  Debian no longer provides the packages (some
    tiff between Oracle and the Debian community).  Second, this provides a
    consistent installation regardless of distribution.

--- packer/scripts/virtualbox.sh
+++ packer/scripts/virtualbox.sh
@@ -9,11 +9,32 @@ if ! $SOURCED; then
   IFS=$'\n\t'
 fi
 
-distro=$(lsb_release -i -s)
+distro=$(lsb_release -i -s | tr '[:upper:]' '[:lower:]')
 
-if [ $distro = "Debian" ]; then
+if [ $distro = "debian" ]; then
   # Need to manually place the startup.nsh file so Debian can boot correctly
   if [ ! -f /boot/efi/startup.nsh ]; then
     echo "\EFI\debian\grubx64.efi" > /boot/efi/startup.nsh
   fi
+
+  DEBIAN_FRONTEND=noninteractive apt-get install -y linux-headers-$(uname -r)
 fi
+
+### Install the guest additions using the ISO
+# NOTE: Why the ISO?  In Debian the guest addition packages are no longer
+# available and while Ubuntu offers them, this provides consistency.
+
+# Get the ISO, this version uses the apt package (usually out of date)
+#DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends virtualbox-guest-additions-iso
+
+# Get the ISO, this version uses the virutalbox site (more up-to-date)
+mkdir /usr/share/virtualbox
+version=$(curl -LSs https://download.virtualbox.org/virtualbox/LATEST-STABLE.TXT)
+curl -LSs -o /usr/share/virtualbox/VBoxGuestAdditions.iso "https://download.virtualbox.org/virtualbox/$version/VBoxGuestAdditions_$version.iso"
+
+# Mount the ISO and run the install
+mkdir /media/vb-additions
+mount /usr/share/virtualbox/VBoxGuestAdditions.iso /media/vb-additions -o loop
+/media/vb-additions/VBoxLinuxAdditions.run --nox11 || true
+umount /media/vb-additions
+rmdir /media/vb-additions

f905ca5abb520438b32941a18620a1454bf898ce
Brennan Fee <brennan+gitlab@mailbots.org>
2022-10-07 19:00:29 -0500

    feat: Debian vagrants working again

--- vagrant/load-vagrant.bash
+++ vagrant/load-vagrant.bash
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Bash strict mode
-# shellcheck disable=SC2154
-([[ -n ${ZSH_EVAL_CONTEXT} && ${ZSH_EVAL_CONTEXT} =~ :file$ ]] ||
- [[ -n ${BASH_VERSION} ]] && (return 0 2>/dev/null)) && sourced=true || sourced=false
-if ! ${sourced}; then
+([[ -n ${ZSH_EVAL_CONTEXT:-} && ${ZSH_EVAL_CONTEXT:-} =~ :file$ ]] ||
+ [[ -n ${BASH_VERSION:-} ]] && (return 0 2>/dev/null)) && SOURCED=true || SOURCED=false
+if ! ${SOURCED}
+then
   set -o errexit # same as set -e
   set -o nounset # same as set -u
   set -o errtrace # same as set -E
@@ -12,16 +12,19 @@ if ! ${sourced}; then
   set -o posix
   #set -o xtrace # same as set -x, turn on for debugging
 
+  shopt -s inherit_errexit
   shopt -s extdebug
   IFS=$(printf '\n\t')
 fi
 # END Bash scrict mode
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+#echo -e "\e[93mThe SCRIPT_DIR - ${SCRIPT_DIR}\e[0m"
 
 function load_box() {
   local BOX_FILE=$1
-  if [[ -f "${SCRIPT_DIR}/boxes/${BOX_FILE}.box" ]]; then
+  if [[ -f "${SCRIPT_DIR}/../images/${BOX_FILE}.box" ]]
+  then
     if vagrant box list | grep -q "${BOX_FILE}"
     then
       echo -e "\e[93mRemoving old box - ${BOX_FILE}\e[0m"
@@ -29,11 +32,10 @@ function load_box() {
     fi
 
     echo -e "\e[93mAdding box - ${BOX_FILE}\e[0m"
-    vagrant box add "${SCRIPT_DIR}/boxes/${BOX_FILE}.box" --name "${BOX_FILE}"
+    vagrant box add "${SCRIPT_DIR}/../images/${BOX_FILE}.box" --name "${BOX_FILE}"
   fi
 }
 
-load_box "bfee-ubuntu-lts-bare"
-load_box "bfee-ubuntu-rolling-bare"
-load_box "bfee-debian-stable-bare"
-load_box "bfee-debian-testing-bare"
+load_box "bfee-vagrantVbox-debian-stable-bare"
+load_box "bfee-vagrantVbox-debian-testing-bare"
+#load_box "bfee-ubuntu-rolling-bare"

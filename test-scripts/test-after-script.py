68562cb6f6b362498d0f022b590c96d6227a7775
Brennan Fee <brennan+gitlab@mailbots.org>
2022-11-14 20:03:33 -0600

    fix: Path issue

--- test-scripts/test-after-script.py
+++ test-scripts/test-after-script.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 import os
 
-with open('/home/usr/install.log', 'a') as the_file:
+with open('/home/user/install.log', 'a') as the_file:
     the_file.write(f"AFTER SCRIPT: Hello from after script." + os.linesep)

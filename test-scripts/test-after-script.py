6e98fac7117c35d4c965b16acd745b656423cdf9
Brennan Fee <brennan+gitlab@mailbots.org>
2022-11-14 20:08:39 -0600

    fix: Adding encoding

--- test-scripts/test-after-script.py
+++ test-scripts/test-after-script.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 import os
 
-with open('/home/user/install.log', 'a') as the_file:
-    the_file.write(f"AFTER SCRIPT: Hello from after script." + os.linesep)
+with open('/home/user/install.log', 'a', encoding="utf-8") as the_file:
+    the_file.write("AFTER SCRIPT: Hello from after script." + os.linesep)
